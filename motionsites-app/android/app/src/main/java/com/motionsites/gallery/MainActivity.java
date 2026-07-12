fdfb4b83eea829f9f670d377a4d9c476d5ee570e
MotionSites Extractor <user@motionsites-extract.local>
2026-07-12 09:36:28 +0000

    feat: 添加 Capacitor Android 项目，一条命令即可打包 APK

---
+++ motionsites-app/android/app/src/main/java/com/motionsites/gallery/MainActivity.java
@@ -0,0 +1,5 @@
+package com.motionsites.gallery;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ motionsites-app/capacitor.config.js
@@ -0,0 +1,13 @@
+const config = {
+  appId: 'com.motionsites.gallery',
+  appName: 'MotionSites Gallery',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  },
+  android: {
+    allowMixedContent: true
+  }
+};
+
+module.exports = config;
